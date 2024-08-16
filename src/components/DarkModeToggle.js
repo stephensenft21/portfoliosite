ace2604d8fe3cc7c733352006aa18aaf669a27de
stephen senft <stephen.senft@senftluxrentals.com>
2024-08-16 14:42:50 -0500

    removed dark mode toggle

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-const DarkModeToggle = () => {
-  const [darkMode, setDarkMode] = useState(false);
+// const DarkModeToggle = () => {
+//   const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, [darkMode]);
+//   useEffect(() => {
+//     if (darkMode) {
+//       document.documentElement.classList.add('dark');
+//     } else {
+//       document.documentElement.classList.remove('dark');
+//     }
+//   }, [darkMode]);
 
-  return (
-    <button 
-      onClick={() => setDarkMode(!darkMode)}
-      className={`p-2 rounded-md focus:outline-none ${darkMode ? 'bg-highlight text-white' : 'bg-secondary text-textPrimary'}`}
-    >
-      {darkMode ? 'Light Mode' : 'Dark Mode'}
-    </button>
-  );
-};
+//   return (
+//     <button 
+//       onClick={() => setDarkMode(!darkMode)}
+//       className={`p-2 rounded-md focus:outline-none ${darkMode ? 'bg-highlight text-white' : 'bg-secondary text-textPrimary'}`}
+//     >
+//       {darkMode ? 'Light Mode' : 'Dark Mode'}
+//     </button>
+//   );
+// };
 
-export default DarkModeToggle;
+// export default DarkModeToggle;
